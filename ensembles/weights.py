665552eb88de5836cdfd9cddc772e1e145e430dc
Matt Amos <m.amos1@lancaster.ac.uk>
2022-06-01 14:24:40 +0100

    Notebook exploring different log-like weighting schemes

--- ensembles/weights.py
+++ ensembles/weights.py
@@ -11,6 +11,7 @@ from tqdm import trange, tqdm
 import numpy as np
 import copy
 import xarray as xr
+import distrax as dx
 
 class AbstractWeight:
     def __init__(self, name: str) -> None:
@@ -34,33 +35,57 @@ class LogLikelihoodWeight(AbstractWeight):
 
     @abc.abstractmethod
     def _compute(
-        self, process_models: ModelCollection, observations: ProcessModel, return_lls=False
+        self, process_models: ModelCollection, observations: ProcessModel, return_lls=False, standardisation_scheme='exp'
     ) -> jnp.DeviceArray:
-        if process_models[0].model_data.ndim > 2:
-             raise NotImplementedError('Not implemented for more than temporal dimensions')
+        # if process_models[0].model_data.ndim > 2:
+        #      raise NotImplementedError('Not implemented for more than temporal dimensions')
 
         assert np.all(process_models.time == observations.time), "Time coordinates do not match between models and observations"
         model_lls = []
         for model in process_models:
-            distribution = model.distribution
+            distribution = model.distribution._dist
             # Expand dims is needed to ensure that the log_prob returns one point per time point
             log_likelihood = lambda x: distribution.log_prob(x)
 
             lls = []
             for obs_real in tqdm(observations):
-                lls.append(log_likelihood(jnp.expand_dims(obs_real.values, -1)))
+                # assert distribution.event_shape == obs_real.values.ravel().shape, 'Observations are not the same size as the model distribution'
+                # This is need because dx.Normal and dx.MultiVariate treat inputs differently
+                if model.distribution.dist_type == dx.Normal:
+                    ll_val = log_likelihood(obs_real.values.ravel())
+                else:
+                    ll_val = log_likelihood(jnp.expand_dims(obs_real.values.ravel(), -1))
+                lls.append(ll_val)
+
             lls_array = jnp.asarray(lls)
             lls_mean = jnp.mean(lls_array, axis=0)
+            # TODO: Question about whether these should be done on the mean or on the individual log-likelihoods?
+            if standardisation_scheme == 'exp':
+                # Exponentially scales - enforces positivity
+                lls_mean = np.exp(lls_mean)
+            # elif standardisation_scheme == 'min-max':
+            #     # Scales everything from 0 to 1
+            #     lls_mean = (lls_mean - np.nanmin(lls_mean)) / (np.nanmax(lls_mean) - np.nanmin(lls_mean))
+            # elif standardisation_scheme == 'subtract-constant':
+            #     # Here this ensures everything is negative
+            #     lls_mean = lls_mean - np.ceil(lls_mean)
             lls_mean_xarray = copy.deepcopy(model.model_data.isel(realisation=0)).drop_vars('realisation')
-            lls_mean_xarray.data = lls_mean
+            lls_mean_xarray.data = lls_mean.reshape(lls_mean_xarray.shape)
             lls_mean_xarray = lls_mean_xarray.assign_coords(model=model.model_name)
+
             model_lls.append(lls_mean_xarray)
 
         # Put weights into an xarray DataArray for continuity and dimension description
         model_lls = xr.concat(model_lls, dim='model')  # (n_reals, time)
-        weights = model_lls / model_lls.sum('model')
+        model_lls = model_lls.rename('Log-likelihoods')
+        model_lls_sum = model_lls.sum('model') # This returns zero where there were nans
+        # model_lls_sum_masked = model_lls_sum.where(model_lls_sum != 0.000) # Need this to replace 0.000s with nans
+        weights = model_lls / model_lls_sum
+        # weights = model_lls / model_lls_sum_masked
 
-        assert weights.shape == (len(process_models), len(observations.time))
+        weights = weights.rename('Log-likelihood weights')
+
+        assert weights.shape == (len(process_models), ) + obs_real.shape
 
         if return_lls:
             return weights, model_lls
